ee82a3e65d91f59367874b993b0a6740f2e7f36d
Agustín Rodríguez <killgt@gmail.com>
2024-02-13 21:57:48 +0100

    primer paso previo a cobrar

---
+++ resources/js/Layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="flex flex-col bg-gray-50 min-h-screen text-white">
+		<div class="container mx-auto bg-white shadow min-h-screen">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,25 @@
-<script setup>
+<script setup lang="ts">
+import Layout from "../Layout.vue";
+import {useFileDialog} from "@vueuse/core";
+import {ref, watch} from "vue";
+
 defineProps({ test: String })
+const { files, open, reset } = useFileDialog({
+	accept: 'image/*',
+});
+const img = ref('');
+
+watch(files, (newFiles: FileList) => {
+	const file = newFiles[0];
+	img.value = URL.createObjectURL(file);
+});
 </script>
 
 <template>
-    Hello from component {{ test }}
+	<Layout>
+		<img :src="img" v-if="img" class="w-16 h-16 object-cover">
+    <button v-if="!img" @click="open" class="bg-white text-black rounded">
+	    Select a file from your computer
+    </button>
+	</Layout>
 </template>
